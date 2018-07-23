fd3f973147430fc090660f5a2d0f25f1baa508c4
cfreyes7 <cfreyes7@gmail.com>
2018-07-23 14:44:01 -0500

    adicion paginas y autenticacion con firebase

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
+# grupo-4-crecer-en-inteligencia-social
+
 # ReportApp
+
 Aplicación movil para realizar reportes sociales en tiempo real
+
+
+### Prerequisitos
+
+Para iniciar debe instalar [Node.js](https://nodejs.org/es/), y posterior a esto instalar
+
+```
+npm install ionic firebase
+```
+### Instalacion
+
+Para iniciar es tan secillo como:
+
+```
+npm install
+```
+
+## Construido con
+
+* [Ionic](https://ionicframework.com/) - Framework Apps
+* [FireBase](https://firebase.google.com/?hl=es-419) - Plataforma para desarrollo web y movil
+
+## Autores
+
+* **Fabian Reyes** - *Initial work* - [cfreyes7](https://github.com/cfreyes7)
+
+## Licencia
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,66 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { Camera } from '@ionic-native/camera';
+import { File } from '@ionic-native/file';
+import { FilePath } from '@ionic-native/file-path';
+import { FileTransfer } from '@ionic-native/file-transfer';
+import { Facebook } from '@ionic-native/facebook'
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { ForgetPage } from '../pages/forget/forget';
+
+//Angular Firebase Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { LoadingProvider } from '../providers/loading/loading';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBxN5IChUAOpD_gIPFCwKPZLo2ZM8Zz9r8",
+  authDomain: "reportapp-9c4a1.firebaseapp.com",
+  databaseURL: "https://reportapp-9c4a1.firebaseio.com",
+  projectId: "reportapp-9c4a1",
+  storageBucket: "reportapp-9c4a1.appspot.com",
+  messagingSenderId: "431560914220"
+}
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    ForgetPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    ForgetPage
   ],
   providers: [
     StatusBar,
+    Camera,
+    File,
+    FilePath,
+    FileTransfer,
+    Facebook,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LoadingProvider
   ]
 })
 export class AppModule {}
